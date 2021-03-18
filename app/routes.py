032bc9d395cce23f3ab106e2176da137381fb8ec
feulo-ocean <guilherme.feulo@oceanbrasil.com>
2021-03-18 23:41:46 +0000

    com registro

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app, db
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import current_user, login_user, logout_user
 from app.models import User
@@ -27,3 +27,20 @@ def login():
         login_user(user, remember=r)
         return redirect(url_for('index'))
     return render_template("login.html" , title="Login")
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    if request.method == "POST":
+        user = User(username=request.values.get('user'),
+                    password=request.values.get('pass'))
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template("register.html" , title="Register")
